b4bbf1df7b1682d113e78ff3d9f444fb0127eb8f
8mileverse <Ayodelu4@gmail.com>
2024-07-30 12:30:31 +0100

    DOM part 3 currently at Create and Append Elements

---
+++ public/55 DOM/55 DOM.js
@@ -0,0 +1,10 @@
+console.log(window.document);
+
+console.dir(window.document);
+
+
+console.log(document.body);
+// console.log(document.body.innerText);
+
+
+// document.querySelector('#test h2').innerText = 'Hi';
